6b62bfc76de9118f13b9472c73b950dd9cb0db09
miaodx <miaodx@hotmail.com>
2017-03-26 20:02:54 +0800

    regular update.

--- stepper/src/control_stepper_with_pin.js
+++ stepper/src/control_stepper_with_pin.js
@@ -25,7 +25,13 @@ new five.Board().on("ready", function() {
     type: "digital",
     mode: five.Pin.INPUT
   });
-
+  
+  const test_enable_pin = new five.Pin({
+    pin: 4,
+    type: "digital",
+    mode: five.Pin.INPUT
+  });
+  
   const stop_pin_1 = new five.Pin({
     pin: 5,
     type: "digital",
@@ -39,17 +45,38 @@ new five.Board().on("ready", function() {
   });
 
   var enable_pin = new five.Pin({
-    pin: 30,
+    pin: 38,
     type: "digital",
     mode: five.Pin.OUTPUT
   });
   
+
+  /**
+  #define X_STEP_PIN         54
+  #define X_DIR_PIN          55
+  #define X_ENABLE_PIN       38
+  #define X_MIN_PIN           3
+  #define X_MAX_PIN           2
+
+  #define Y_STEP_PIN         60
+  #define Y_DIR_PIN          61
+  #define Y_ENABLE_PIN       56
+  #define Y_MIN_PIN          14
+  #define Y_MAX_PIN          15
+
+  #define Z_STEP_PIN         46
+  #define Z_DIR_PIN          48
+  #define Z_ENABLE_PIN       62
+  #define Z_MIN_PIN          18
+  #define Z_MAX_PIN          19
+   */
+
   const stepper_1 = new five.Stepper({
     type: five.Stepper.TYPE.DRIVER,
     stepsPerRev: 200,
     pins: {
-      step: 36,
-      dir: 34
+      step: 54,
+      dir: 55
     }
   });  
 
@@ -61,7 +88,7 @@ new five.Board().on("ready", function() {
     
     // stepper_decorate_1.move(20000);
     
-    let status = await stepper_decorate_1.async_move(20000);
+    let status = await stepper_decorate_1.async_move(2000);
     console.log("We got status: ", status);
     
     // stepper_decorate_1.promise_move(4000)
@@ -75,6 +102,16 @@ new five.Board().on("ready", function() {
   });
 
 
+  // pin 4
+  test_enable_pin.on("high", function() {
+    console.log("Enable the enable_pin");
+    // stepper_decorate_1.stop();
+    enable_pin.low();
+    console.log("Enable_pin enabled !!");
+  });
+
+
+
   // pin 5
   stop_pin_1.on("high", function() {
     console.log("Stop pin is high, Just stop!!");
@@ -82,7 +119,7 @@ new five.Board().on("ready", function() {
     console.log("Stop Done!!");
   });
 
-  // pin6`
+  // pin6
   stop_pin_2.on("high", function() {
     console.log("Limit STOP PIN IS HIGH!");
     stepper_decorate_1.trigger_stop_by_limit(stop_pin_2.pin);
